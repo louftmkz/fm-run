6c4ba0e047ecba000d7359104688c4e4aa00f136
Lou <lou.kailich@acture.de>
2026-05-29 18:14:51 +0000

    Step 57: Ramp-Sprite Level 1 zurueck + Best-Anzeige sichtbarer + SW v33
    - LEVEL_RAMP_SPRITES[2] = './sprites/ramp-level-1.png' (Training Room Sprite wieder aktiv)
      User bestaetigt: Sprite war nicht der Lou-Climb-Bug. Step 56 (Force-Refresh) hat den
      Cache-Stand auf den deployed state gebracht und alles funktioniert wieder.
    - .best Anzeige: font-size 7px -> 9px, color #a9b3d8 statt --ink-dim (heller),
      text-shadow fuer Lesbarkeit gegen jeden Background.
    - Service Worker v32 -> v33.

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 // Strategy: cache-first für statische Assets, network-first für HTML (damit Updates beim
 // neuen Deploy schnell durchkommen).
 
-const CACHE_NAME = 'fm-run-v32';
+const CACHE_NAME = 'fm-run-v33';
 const PRECACHE = [
   './',
   './index.html',
