88725bb0478c31dc83e92877b0e13fcd658874e2
Lou Kailich <lou.kailich@acture.de>
2026-05-30 19:09:12 +0000

    Step 67: Layout body-flex (Bottom-Space fix), Ramp-Buffer 500, Obstacle native*1.5, .best nowrap, .top BG

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 // Strategy: cache-first für statische Assets, network-first für HTML (damit Updates beim
 // neuen Deploy schnell durchkommen).
 
-const CACHE_NAME = 'fm-run-v42';
+const CACHE_NAME = 'fm-run-v43';
 const PRECACHE = [
   './',
   './index.html',
