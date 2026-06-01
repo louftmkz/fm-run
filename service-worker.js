63779b617b9dc65e1573256cde843b55c776e3ef
Lou Kailich <lou.kailich@acture.de>
2026-06-01 23:14:54 +0000

    Step 83: Settings-Title left-aligned (modal-uebergreifend konsistent) + OK-Button zurueck (X=Cancel, OK=Confirm)

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 // Strategy: cache-first für statische Assets, network-first für HTML (damit Updates beim
 // neuen Deploy schnell durchkommen).
 
-const CACHE_NAME = 'fm-run-v58';
+const CACHE_NAME = 'fm-run-v59';
 const PRECACHE = [
   './',
   './index.html',
