8361a6f1533bddb9a889aeb332faf3514079f69d
Lou Kailich <lou.kailich@acture.de>
2026-06-02 21:54:39 +0000

    Step 89: Training Room Obstacle-Hitbox halbieren (Code-Level 2, sw/4 Inset)

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 // Strategy: cache-first für statische Assets, network-first für HTML (damit Updates beim
 // neuen Deploy schnell durchkommen).
 
-const CACHE_NAME = 'fm-run-v64';
+const CACHE_NAME = 'fm-run-v65';
 const PRECACHE = [
   './',
   './index.html',
